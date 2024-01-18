31d8e0402b6dd4b34f3411af8d985587742ba208
Pierre Seznec <petterseznec@gmail.com>
2024-01-18 17:57:25 +0100

    display nodes version count

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
+import { connectedNodes, nodeInfos } from "./types";
 
-type connectedNodes = {
-  [key: string]: (string | boolean)[];
-};
+const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
+
+const nodes: Record<string, nodeInfos> = {};
 
 async function fetchConnectedNodes(
   url: string
 ): Promise<connectedNodes | undefined> {
+  let connectedNodes;
+  let version;
   try {
     console.log(`Fetching connected nodes from ${url}...`);
     const response = await axios.post(
@@ -19,69 +22,71 @@ async function fetchConnectedNodes(
       },
       { timeout: 1000 }
     );
-
-    const connectedNodes = response.data.result.connected_nodes;
-    console.log(`found ${Object.keys(connectedNodes).length} connected nodes}`);
-
+    connectedNodes = response.data.result.connected_nodes;
+    version = response.data.result.version;
     return connectedNodes;
   } catch (error) {
-    console.error(
-      `Error fetching connected nodes of ${url}:`,
-      error?.toString()
-    );
+    // console.error(
+    //   `Error fetching connected nodes of ${url}:`,
+    //   error?.toString()
+    // );
+  } finally {
+    nodes[url] = { version: version, routable: !!connectedNodes };
   }
 }
 
-async function crawlBlockchainNode(
-  endpoint: string,
-  visitedNodes: Set<string>
-): Promise<{ routables: number; unreachable: number }> {
-  if (visitedNodes.has(endpoint)) {
-    return { routables: 0, unreachable: 0 }; // Avoid revisiting nodes
+async function crawlBlockchainNode(url: string): Promise<void> {
+  if (Object.keys(nodes).some((visitedUrl) => visitedUrl === url)) {
+    return;
   }
 
-  visitedNodes.add(endpoint);
+  nodes[url] = { version: undefined, routable: false };
 
-  const connectedNodes = await fetchConnectedNodes(endpoint);
+  const connectedNodes = await fetchConnectedNodes(url);
 
   if (!connectedNodes) {
-    return { routables: 0, unreachable: 1 };
+    return;
   }
-  let totalUnreachable = 0;
 
-  const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
-
-  let totalNodes = 1; // Count the current node
   await Promise.all(
     Object.values(connectedNodes).map(async (node) => {
       const ip = node[0] as string;
       const isIpv4 = ipv4Pattern.test(ip);
 
       const url = isIpv4 ? `http://${ip}:33035` : `http://[${ip}]:33035`;
-      const { routables, unreachable } = await crawlBlockchainNode(
-        url,
-        visitedNodes
-      );
-      totalNodes += routables;
-      totalUnreachable += unreachable;
+      await crawlBlockchainNode(url);
     })
   );
-
-  return { routables: totalNodes, unreachable: totalUnreachable };
 }
 
 async function main() {
   const initialEndpoint = "https://mainnet.massa.net/api/v2";
 
-  const nodeSet = new Set<string>();
-  const { routables, unreachable } = await crawlBlockchainNode(
-    initialEndpoint,
-    nodeSet
+  await crawlBlockchainNode(initialEndpoint);
+
+  console.log(
+    `Number of known nodes in the network: ${Object.keys(nodes).length}`
+  );
+  console.log(
+    `Number of routables: ${
+      Object.values(nodes).filter((node) => node.routable).length
+    }`
+  );
+  console.log(
+    `Number of unreachable: ${
+      Object.values(nodes).filter((node) => !node.routable).length
+    }`
   );
 
-  console.log(`Number of known nodes in the network: ${nodeSet.size}`);
-  console.log(`Number of routables: ${routables}`);
-  console.log(`Number of unreachable: ${unreachable}`);
+  let versions: Record<string, number> = {};
+  for (const nodeInfo of Object.values(nodes)) {
+    if (nodeInfo.version) {
+      versions[nodeInfo.version]
+        ? versions[nodeInfo.version]++
+        : (versions[nodeInfo.version] = 1);
+    }
+  }
+  console.log(`Versions:`, versions);
 }
 
 main();
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type connectedNodes = {
+    [key: string]: (string | boolean)[];
+  };
+  
+  export  type nodeInfos = {
+    version: string | undefined;
+    routable: boolean;
+  }
