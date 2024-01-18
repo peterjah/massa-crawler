56711e65a4417d8059bed26f39e2ffc1e93e14f8
Pierre Seznec <petterseznec@gmail.com>
2024-01-19 00:47:02 +0100

    remove duplicates from ipv6 mapped ipv4

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ import axios from "axios";
 import { connectedNodes, nodeInfos } from "./types";
 
 const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/;
+const ipv6MappedIpv4Regex = /^::ffff:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/;
 
 const nodes: Record<string, nodeInfos> = {};
 
-async function getNodeInfos(
-  url: string
-): Promise<connectedNodes | undefined> {
-  let connectedNodes;
-  let version;
+async function getNodeInfos(url: string): Promise<connectedNodes | undefined> {
   try {
     console.log(`Fetching infos from ${url}...`);
     const response = await axios.post(
@@ -22,16 +19,15 @@ async function getNodeInfos(
       },
       { timeout: 1000 }
     );
-    connectedNodes = response.data.result.connected_nodes;
-    version = response.data.result.version;
-    return connectedNodes;
+
+    nodes[url].version = response.data.result.version;
+    nodes[url].routable = true;
+    return response.data.result.connected_nodes;
   } catch (error) {
     // console.error(
     //   `Error fetching connected nodes of ${url}:`,
     //   error?.toString()
     // );
-  } finally {
-    nodes[url] = { version, routable: !!version };
   }
 }
 
@@ -53,30 +49,34 @@ async function visitNode(url: string): Promise<void> {
     Object.values(connectedNodes).map(async (node) => {
       const ip = node[0] as string;
       const isIpv4 = ipv4Pattern.test(ip);
+      const ipv6MappedMatch = ip.match(ipv6MappedIpv4Regex);
 
-      const url = isIpv4 ? `http://${ip}:33035` : `http://[${ip}]:33035`;
+      const url = ipv6MappedMatch
+        ? `http://${ipv6MappedMatch[1]}:33035`
+        : isIpv4
+        ? `http://${ip}:33035`
+        : `http://[${ip}]:33035`;
       await visitNode(url);
     })
   );
 }
 
 async function main() {
-  const initialEndpoint = "https://mainnet.massa.net/api/v2";
+  const initialEndpoint = "http://144.24.196.246:33035";
 
   await visitNode(initialEndpoint);
 
   console.log(
-    `Number of known nodes in the network: ${Object.keys(nodes).length}`
+    "\nNumber of known nodes in the network:",
+    Object.keys(nodes).length
   );
   console.log(
-    `Number of routables: ${
-      Object.values(nodes).filter((node) => node.routable).length
-    }`
+    "Number of routables:",
+    Object.values(nodes).filter((node) => node.routable).length
   );
   console.log(
-    `Number of unreachable: ${
-      Object.values(nodes).filter((node) => !node.routable).length
-    }`
+    "Number of unreachable:",
+    Object.values(nodes).filter((node) => !node.routable).length
   );
 
   let versions: Record<string, number> = {};
